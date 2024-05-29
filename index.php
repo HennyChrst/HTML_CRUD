cc9b68c32c8c123875fa0fc418a28bafb5f05557
HennyChrst <hennyinosensia@gmail.com>
2024-05-29 09:35:05 +0700

    upgrade aplikasi

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Perpustakaan</title>
+    <title>APLIKASI PERPUSTAKAAN VER 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-    <h1 class="my-4">Aplikasi Perpustakaan</h1>
+    <h1 class="my-4">Aplikasi Perpustakaan Versi 3</h1>
     <a class="btn btn-primary" href="./buku.php">Lihat Daftar Buku</a>
     <a class="btn btn-success" href="./staff.php">Lihat Daftar Staff</a>
     <!-- form logout -->
